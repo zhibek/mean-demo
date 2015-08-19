5602435498d47c824ab38690f0184ae7dcfeadc9
John Levermore <john.levermore@zhibek.com>
2015-08-19 16:54:59 +0200

    Add RESTful service to manage Posts & Comments (including initialising Mongo DB, creating Mongo models, creating Express routes/actions).

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var mongoose = require('mongoose');
+require('./models/Posts');
+require('./models/Comments');
+
+mongoose.connect('mongodb://localhost/news');
+
 var routes = require('./routes/index');
 
 var app = express();
